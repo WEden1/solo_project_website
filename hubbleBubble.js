c1ead25ec0ee8c39e7e6b546decf8f46c286d31b
William Eden <williameden@gmail.com>
2019-09-08 22:33:46 +0100

    first commit

---
+++ hubbleBubble.js
@@ -0,0 +1,20 @@
+function submitHandler(form) {
+    let ingredients = [];
+
+    for(element of form.elements){
+        ingredients[element.id] = element.value;
+    }
+
+    const req = new XMLHttpRequest();
+    req.open('POST', )
+    req.onload = () => {
+
+        if (req.status >= 200 && req.status <= 300) {
+            console.log("success");
+        } else {
+            console.log("Fail!!")
+        }
+    }
+    req.setRequestHeader();
+    req.send(JSON.stringify(ingredients));
+}
