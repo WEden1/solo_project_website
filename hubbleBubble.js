ab640e06c479e07755b2982f429dd7b99a3fa570
??? <???>
2019-09-16 15:25:52 +0100

    Post request added

--- hubbleBubble.js
+++ hubbleBubble.js
@@ -7,17 +7,17 @@ function submitHandler(form) {
         ingredients[element.id] = element.value;
     }
 
-    // const req = new XMLHttpRequest();
-    // req.open('POST', )
-    // req.onload = () => {
+    const req = new XMLHttpRequest();
+    req.open('POST', "http://35.204.118.105:9000/ingredient")
+    req.onload = () => {
 
-    //     if (req.status >= 200 && req.status <= 300) {
-    //         console.log("success");
-    //     } else {
-    //         console.log("Fail!!")
-    //     }
-    // }
-    // req.setRequestHeader();
-    // req.send(JSON.stringify(ingredients));
+        if (req.status >= 200 && req.status <= 300) {
+            console.log("success");
+        } else {
+            console.log("Fail!!")
+        }
+    }
+    req.setRequestHeader();
+    req.send(JSON.stringify(ingredients));
     return false;
 }
